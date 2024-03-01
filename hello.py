5fb33713baeb38c0813ad35051444b833f18dad0
senthamil18 <senthamil.r25@gmail.com>
2024-03-02 00:26:24 +0530

    update: operators and output

--- hello.py
+++ hello.py
@@ -3,24 +3,24 @@ print("Subtraction - 2")
 print("Multiplication - 3")
 print("Division - 4")
 print("Remainder - 5")
-z=int(input("Enter the mode of Operation : ")
+z=int(input("Enter the mode of Operation : "))
 a=int(input("Enter the First value: "))
 b=int(input("Enter the Second Value: "))
-
 c=a+b
 d=a-b
 e=a*b
 f=a/b
 g=a//b
-
-if(z=1):
+if(z==1):
   print("The Result Value is : ",c)
-elif(z=2):
+elif(z==2):
   print("The Result Value is : ",d)
-elif(z=3):
+elif(z==3):
   print("The Result Value is : ",e)
-elif(z=4):
-  print("The Result VAlue is : ",f)
+elif(z==4):
+  print("The Result Value is : ",f)
+elif(z==5):
+  print("The Result Value is : ",g)
+else:
+  print("Enter the proper operator")
   
-
-print("The Result value is : ",c)
